6f82c8258f1dd1fdd3e47884e805d7d7975d938c
Mohib <mohib7870@gmail.com>
2022-02-10 11:15:16 +0530

    Updated all files x 1

--- index.js
+++ index.js
@@ -1,46 +1,80 @@
-let month = document.getElementById("month");
-let date = document.getElementById("date");
-let year = document.getElementById("year");
-let todayMonth = document.getElementById("todayMonth");
-let todayDate = document.getElementById("todayDate");
-let todayYear = document.getElementById("todayYear");
+const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
+function ageCalculate() {
+  let birthInputDate = new Date(
+    document.getElementById("birth-date-input").value
+  );
+  let CurrentInputDate = new Date(
+    document.getElementById("current-date-input").value
+  );
+  let calculated_month, calculated_date, calculated_year;
 
-function myFunction () {
-   
-  let yearSathi = todayYear.value-year.value;
-  let monthSathi = todayMonth.value-month.value;
-  let daySathi = todayDate.value - date.value;
-  let eDays = 31 + daySathi;
-  let eMonths = 12 + monthSathi;
-  if(yearSathi>=0){
-      if(monthSathi>=0){
-        if(daySathi>=0){
-          let a = "Your Age is \n " + yearSathi + " Years " + monthSathi + " Months " + daySathi + " Days ";
-          if(yearSathi>=0 && monthSathi>=0){
-            document.getElementById("result").innerHTML = a;
-          }
-          else document.getElementById("result").innerHTML="Date of birth needs to be earlier than the age at date";
-        }
-        else{
-          let a = "Your Age is \n " + yearSathi + " Years " + (monthSathi-1) + " Months " + eDays + " Days ";
-          if(yearSathi>=0){
-            document.getElementById("result").innerHTML = a;
-          }
-          else document.getElementById("result").innerHTML="Date of birth needs to be earlier than the age at date";
-        }
-      }
-      else{
-        let a = "Your Age is \n " + (yearSathi-1) + " Years " + eMonths + " Months " + daySathi + " Days ";
-        if(yearSathi>=0){
-          document.getElementById("result").innerHTML = a;
-        }
-        else document.getElementById("result").innerHTML="Date of birth needs to be earlier than the age at date";
-      }
-  }else document.getElementById("result").innerHTML="Date of birth needs to be earlier than the age at date";
-  
+  let birthDetails = {
+    birthDate: birthInputDate.getDate(),
+    birthMonth: birthInputDate.getMonth() + 1,
+    birthYear: birthInputDate.getFullYear(),
+  };
+  let currentdetails = {
+    currentDate: CurrentInputDate.getDate(),
+    currentMonth: CurrentInputDate.getMonth() + 1,
+    currentYear: CurrentInputDate.getFullYear(),
+  };
+
+  leapChecker(currentdetails.currentYear);
+
+  if (
+    birthDetails.birthYear > currentdetails.currentYear ||
+    (birthDetails.birthMonth > currentdetails.currentMonth &&
+      birthDetails.birthYear == currentdetails.currentYear) ||
+    (birthDetails.birthDate > currentdetails.currentDate &&
+      birthDetails.birthMonth == currentdetails.currentMonth &&
+      birthDetails.birthYear == currentdetails.currentYear)
+  ) {
+    alert("NOT BORN YET");
+    return;
+  }
+
+  calculated_year = currentdetails.currentYear - birthDetails.birthYear;
+
+  if (currentdetails.currentMonth >= birthDetails.birthMonth) {
+    calculated_month = currentdetails.currentMonth - birthDetails.birthMonth;
+  } else {
+    calculated_year--;
+    calculated_month =
+      12 + currentdetails.currentMonth - birthDetails.birthMonth;
+  }
+
+  if (currentdetails.currentDate >= birthDetails.birthDate) {
+    calculated_date = currentdetails.currentDate - birthDetails.birthDate;
+  } else {
+    calculated_month--;
+    let days = months[currentdetails.currentMonth - 2];
+    calculated_date =
+      days + currentdetails.currentDate - birthDetails.birthDate;
+    if (calculated_month < 0) {
+      calculated_month = 11;
+      calculated_year--;
+    }
+  }
+  displayResult(calculated_date, calculated_month, calculated_year);
 }
+function displayResult(bDate, bMonth, bYear) {
+  document.getElementById("years").textContent = bYear;
+  document.getElementById("months").textContent = bMonth;
+  document.getElementById("days").textContent = bDate;
+}
+function leapChecker(year) {
+  if (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)) {
+    months[1] = 29;
+  } else {
+    months[1] = 28;
+  }
+}
+function reset(){
+    document.getElementById("years").innerHTML = "-";
+    document.getElementById("months").innerHTML = "-";
+    document.getElementById("days").innerHTML = "-";
+    document.getElementById("birth-date-input").value = "";
+    document.getElementById("current-date-input").value = "";
 
-function rest() {
-  document.getElementById("result").innerHTML ="";
 }
